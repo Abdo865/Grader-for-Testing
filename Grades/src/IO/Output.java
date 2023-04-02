6f43beff602ade06afb4fbfaba4d3c1aa1f89a71
Sar00-Ahmed <sarasara6612@yahoo.com>
2023-04-02 22:44:50 +0200

    added student and subject classes and both are ready to be used

--- Grades/src/IO/Output.java
+++ Grades/src/IO/Output.java
@@ -3,49 +3,48 @@ package IO;
 import Info.Student;
 import Info.Subject;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.File;
 import java.util.ArrayList;
 
 public class Output {
     private ArrayList<Student> data;
     private Subject subject;
+    private File file;
 
-    public Output(Subject subject) {
-        File file = new File("../"+subject.getSubjectName() + ".CSV");
+    public Output(Subject subject) throws IOException{
+
+        this.file = new File("../" + subject.getSubjectName() + ".CSV");
         //create the file
-        try {
-            this.subject = subject;
-            if (!file.createNewFile()) throw new IOException("File already exists.");
-        } catch (IOException e) {
-            System.out.println(e);
-        }
+
+        this.subject = subject;
+        if (!file.createNewFile()) throw new IOException("File already exists.");
+
     }
 
     public void setData(ArrayList<Student> data) {
         this.data = data;
     }
 
-    public void save() {
-        try {
-            if (subject == null) throw new NullPointerException("File not created");
-            else if (data == null) throw new NullPointerException("No entered data");
+    public void save() throws IOException, NullPointerException {
 
-            //write content
-            FileWriter writer = new FileWriter("../"+subject.getSubjectName() + ".CSV");
-            writer.write("Subject Name: " + subject.getSubjectName() + ",Max Mark: " + subject.getFullMark() + "\r\n");
-            writer.write("Student name,Student number,GPA,Grade\r\n");
-            for (Student studentInfo : data) {
-                writer.write(studentInfo.getName() + ",");
-                writer.write(studentInfo.getNumber() + ",");
-                writer.write(studentInfo.getGPA() + ",");
-                writer.write(studentInfo.getGrade() + "\r\n");
-            }
+        if (data == null) throw new NullPointerException("No entered data");
+        FileWriter writer = new FileWriter(file);
+        //write content
 
-            writer.close();
-        } catch (NullPointerException | IOException e) {
-            System.out.println(e);
+        writer.write("Subject Name: " + subject.getSubjectName() + ",Max Mark: " + subject.getFullMark() + "\r\n");
+        writer.write("Student name,Student number,GPA,Grade\r\n");
+        for (Student studentInfo : data) {
+            writer.write(studentInfo.getName() + ",");
+            writer.write(studentInfo.getNumber() + ",");
+            writer.write(studentInfo.getGPA() + ",");
+            writer.write(studentInfo.getGrade() + "\r\n");
         }
+    }
+
+    public void save(ArrayList<Student> data) throws IOException, NullPointerException {
+        setData(data);
+        save();
     }
 }
